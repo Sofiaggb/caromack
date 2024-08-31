91e3f20261d5d6304a25242fa9f28a839acb0fd9
sofiaggb <sofiaggb@gmail.com>
2024-08-31 16:41:12 -0400

    add redirect routes 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // base:"https://Sofiaggb.github.io/caromack"
-  // build: {
-  //   outDir: 'dist',
-  // },
-  // server: {
-  //   historyApiFallback: true,
-  // },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
